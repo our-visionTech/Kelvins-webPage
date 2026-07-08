48cd56a14e10b13a6121261ee108f103630fe3e4
Krlvn <kadzenje.kelvin6@gmail.com>
2026-07-08 11:07:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,21 @@ window.addEventListener("scroll", function(){
         btn.classList.remove("show");
     }
 });
+
+const reveals = document.querySelectorAll(
+    ".reveal-left, .reveal-right, .reveal-up"
+);
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("reveal-active");
+        }
+
+    });
+},{
+    threshold:0.2
+});
+
+reveals.forEach(el => observer.observe(el));
